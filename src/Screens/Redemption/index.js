35922be995d0b17011e1a24585ef163c5af51b22
FenaliEnacton <fhkhajanchiwala@enacton.com>
2021-05-18 17:37:52 +0530

    amount dropdown on redemption screen

--- src/Screens/Redemption/index.js
+++ src/Screens/Redemption/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import { View, Text, StyleSheet, Dimensions, FlatList, TouchableOpacity, Image, ScrollView } from 'react-native'
 import {
     Header,
@@ -13,12 +13,16 @@ import { BgImage } from '@components/generic'
 import Icon from 'react-native-vector-icons/AntDesign';
 import DownIcon from 'react-native-vector-icons/Feather';
 import LinearButton from '../../Components/LinearButton'
+import ModalDropdown from 'react-native-modal-dropdown';
 
 const height = Dimensions.get('window').height;
 const width = Dimensions.get('window').width;
 
 const Redemption = ({ navigation }) => {
     const [modalVisible, setModalVisible] = useState(false)
+    const [selectedAmt, setselectedAmt] = useState('')
+    const [checkAmt, setCheckAmt] = useState(false)
+    const subject = useRef('')
     const data = [
         {
             id: 1,
@@ -36,13 +40,21 @@ const Redemption = ({ navigation }) => {
             method: [1, 2, 3, 4]
         }
     ]
+    const amount = ['10$', '20$', '30$', '40$']
     const modalToggle = () => {
         setModalVisible(!modalVisible)
     }
+
+
+    const handleSubmit = () => {
+        setCheckAmt(!checkAmt)
+        //navigation.navigate('FavBrand')
+    }
+
     return (
         <BgImage>
             <Header>
-                <HeaderLeft onPress={navigation.goBack()}>
+                <HeaderLeft onPress={() => { navigation.goBack() }}>
                     <Icon name={'left'} size={20} color={Theme.COLORS.white} />
                 </HeaderLeft>
                 <Text style={styles.titleText}>{translate('redeemCoins')}</Text>
@@ -88,17 +100,45 @@ const Redemption = ({ navigation }) => {
                     <View style={styles.modalContentView}>
                         <View style={styles.modalMethodCard}></View>
                         <Text style={styles.methodText}>Nike Gift Card</Text>
-                        <TouchableOpacity style={styles.amountDropdown}>
-                            <Text style={styles.greyText}>Select Amount</Text>
+
+
+                        <TouchableOpacity style={[styles.amountDropdown,
+                        selectedAmt ? { borderColor: Theme.COLORS.primary, borderWidth: 0.5 }
+                            : null
+                        ]}>
+                            <ModalDropdown
+                                ref={subject}
+                                options={amount}
+                                defaultValue={'Select Amount'}
+                                style={styles.dropdownModalView}
+                                textStyle={styles.greyText}
+                                dropdownStyle={styles.dropdownValStyle}
+                                dropdownTextStyle={styles.dropdownText}
+                                dropdownTextHighlightStyle={{ color: Theme.COLORS.black }}
+                                onSelect={(i, val) => {
+                                    setselectedAmt(val);
+                                }}
+                            >
+                            </ModalDropdown>
                             <DownIcon name='chevron-down' size={20} color={Theme.COLORS.white} />
                         </TouchableOpacity>
-                        <LinearButton notActive={true} style={{ marginTop: 20 }} >
+
+                        {!selectedAmt && checkAmt ? <Text style={styles.errorMessage}>{translate('requiredField')}</Text> : null}
+
+                        <LinearButton
+                            notActive={selectedAmt ? false : true}
+                            style={{ marginTop: 20 }}
+                            onPress={() => { handleSubmit() }}>
                             <Text style={styles.btnText}>{translate('continue')}</Text>
                         </LinearButton>
+
                         <Text style={styles.policyText}>{translate('privacyConfirmation')}</Text>
                     </View>
+
                 </View>
             </TransparentModal>
+
+
         </BgImage>
     )
 }
@@ -114,12 +154,43 @@ const styles = StyleSheet.create({
         marginTop: 15,
         width: (width * 75) / 100
     },
+    errorMessage: {
+        ...Theme.appStyle.errorMessage,
+        marginTop: 5,
+        alignSelf: 'center',
+        width: width - 20,
+        textAlign: 'right',
+    },
+    pickerStyle: {
+        position: 'absolute',
+        alignSelf: 'center',
+        backgroundColor: 'red'
+    },
+    dropdownModalView: {
+        width: '90%',
+        height: '100%',
+        justifyContent: 'center'
+    },
+    dropdownValStyle: {
+        width: (width * 85) / 100,
+        //height: 50,
+        marginLeft: -10,
+        borderBottomLeftRadius: 10,
+        borderBottomRightRadius: 10,
+        //justifyContent: 'center',
+        //alignItems: 'center'
+    },
+    dropdownText: {
+        ...Theme.fontStyles.h3Regular,
+        color: Theme.COLORS.grey,
+        textAlign: 'center'
+    },
     btnText: {
         ...Theme.fontStyles.h3Bold
     },
     greyText: {
-        ...Theme.COLORS.h4Regular,
-        color: Theme.COLORS.grey
+        ...Theme.COLORS.h3Regular,
+        color: Theme.COLORS.white
     },
     modalHeader: {
         width: width,
@@ -142,11 +213,12 @@ const styles = StyleSheet.create({
     },
     amountDropdown: {
         height: 45,
+        flex: 1,
         width: (width * 85) / 100,
         borderRadius: 10,
         borderWidth: 1,
         borderColor: Theme.COLORS.grey,
-        marginTop: 50,
+        marginTop: 40,
         flexDirection: 'row',
         paddingHorizontal: 10,
         alignItems: 'center',
